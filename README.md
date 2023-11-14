cc8f3730a87b0ce6f732290f7dadaa38585c9a52
Oscar Neira Garcia <oneirag@yahoo.es>
2023-11-14 16:33:10 +0100

    Fix README.md

--- README.md
+++ README.md
@@ -8,4 +8,35 @@ It is mainly meant for windows (although it also works in macos)
 
 ## Configuration
 
-In order for the software to work
+In order for the software to work, a .cfg file should be created with the name `~./ong_office365.cfg` or `ong_office365.cfg` in current directory.
+
+Example content of the file:
+
+```ini
+[DEFAULT]
+# Leave empty to get email from outlook in windows
+email = contoso@your-tenant-name.com
+# Use tenant from email. Typically is your email domain address
+tenant = your-tenant-name
+# Example of a sharepoint site
+sharepoint = https://${tenant}.sharepoint.com/sites/{site}
+# Example of a sharepoint personal site (onedrive)
+# sharepoint = https://${tenant}-my.sharepoint.com/personal/{your_parsed_email}
+# client_id should come from https://go.microsoft.com/fwlink/?linkid=2083908
+# This is a sample value from a google search 
+client_id = 6731de76-14a6-49ae-97bc-6eba6914391e
+```
+In order to run tests, an additional section "tests" would be required
+```ini
+[tests]
+client_ids =
+        client_id1
+        client_id2
+# Urls of files in the sharepoint that will be test to be downloaded
+relative_urls =
+        /sites/{site-name}/Shared Documents/{folder1}/{folder2/filename1
+        /sites/{site-name}/Shared Documents/{folder1}/{folder2/filename2
+# Location where sample file will be uploaded
+dest_url = Shared Documents/{folder1}/{folder2}
+
+```
--- src/ong_office365/__init__.py
+++ src/ong_office365/__init__.py
@@ -17,12 +17,34 @@ def get_email(default: str = None) -> str:
         return input("Email address: ")
 
 
-config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation(),
-                                   )
-config.read([os.path.expanduser(f'~/.{name}'),
-             os.path.join(os.path.dirname(__file__), f'{name}.cfg'),
-             f'{name}.cfg',
-             ],
-            encoding='utf-8')
+def init_config() -> configparser.ConfigParser:
+
+    default = {
+        "email": "someone@contoso.com",
+        "tenant": "contoso",
+        # client_id should come from https://go.microsoft.com/fwlink/?linkid=2083908
+        # This is a sample value from a google search
+        "client_id": "6731de76-14a6-49ae-97bc-6eba6914391e",
+        "sharepoint": "https://contoso.sharepoint.com/sites/example_site",
+    #    "onedrive": None,   # no need for a value here
+    }
+
+
+
+    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation(),
+                                       )
+    config['DEFAULT'] = default
+
+    # overwrite default values with values in home, in this folder or in current folder
+    config.read([os.path.expanduser(f'~/.{name}'),
+                 os.path.join(os.path.dirname(__file__), f'{name}.cfg'),
+                 f'{name}.cfg',
+                 ],
+                encoding='utf-8')
+
+    return config
+
+
+config = init_config()
 site = config.get("DEFAULT", "sharepoint")
 email = get_email(config.get("DEFAULT", "email"))
